a6d0105f3befd346b6f31a9e12a1d00110f95848
Biblioteca Vila Vitória <67565465+escola-vila-vitoria@users.noreply.github.com>
2024-08-28 22:28:14 -0300

    questoes

---
+++ adivinhas/adivinha1/adivinha1.js
@@ -0,0 +1,23 @@
+var clickCount = 0
+
+function questionClicked(){
+  clickCount = clickCount + 1;
+  
+  if( clickCount > 3 ){
+document.getElementById('answer9').style.visibility='visible'
+    clickCount = 0;
+  }
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+	color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+} 
+
+function memeClicked(){
+document.getElementById('meme1').style.color= getRandomColor();
+}
